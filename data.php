195e0644da30d688a0c7081d4539679cefd7f99b
andnp <soundguyandyp@gmail.com>
2015-04-22 21:47:56 -0400

    Catch up

--- data.php
+++ data.php
@@ -1,24 +1,11 @@
 <?php
 $data = file_get_contents("php://input");
-$arrays = json_decode($data, true);
+$json = json_decode($data, true);
 $f = fopen('data.csv', 'w');
 
-$firstLineKeys = false;
-foreach ($arrays as $line)
-{
-    if (empty($firstLineKeys))
-    {
-        $firstLineKeys = array_keys($line);
-        fputcsv($f, $firstLineKeys);
-        $firstLineKeys = array_flip($firstLineKeys);
-    }
-    $line_array = array($line['id']);
-    $line_array.push($line['phase']);
-	    foreach ($line['data'] as $value)
-    {
-        $line_array.push($value);
-    }
-    fputcsv($f, $line_array);
+fputcsv($f, $json->id);
+fputcsv($f, $json->phase);
 
-}
+
+print_r(error_get_last());
 ?>
