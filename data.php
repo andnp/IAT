4ec6d238a8e5afbf99cca4044ab1f72aff15380c
andnp <soundguyandyp@gmail.com>
2015-04-22 21:55:31 -0400

    catch up

--- data.php
+++ data.php
@@ -3,12 +3,9 @@ $data = file_get_contents("php://input");
 $json = json_decode($data, true);
 $f = fopen('data.csv', 'w');
 
-<<<<<<< HEAD
 fputcsv($f, $json->id);
 fputcsv($f, $json->phase);
 
-=======
->>>>>>> e49087ea7ffb76df92330bba40c66f5b3034379e
 $line_array = [];
 array_push($line_array, $json->"id");
 array_push($line_array, $json->"phase");
@@ -18,8 +15,4 @@ foreach($json->"data" as $value){
 print_r($line_array);
 fputcsv($f, $line_array);
 fclose($f);
-<<<<<<< HEAD
-=======
-
->>>>>>> e49087ea7ffb76df92330bba40c66f5b3034379e
 ?>
