455ec9d08dea8464c9a7747294b4ae8312e3e252
andnp <soundguyandyp@gmail.com>
2015-04-28 20:43:30 -0400

    add remote access to .js files

--- iat.js
+++ iat.js
@@ -107,7 +107,7 @@ function evaluate(picked){
 	waiting_for_input = 0; // clear the waiting for input flag
 	if(counter < TRIALS){ // check if we have completed correct number of trials
 		if(correct(picked)){
-			setTimeout(iat, 700); // call iat function in .75 to 2 seconds
+			setTimeout(iat, 700); // call iat function in .7 seconds
 		} else {
 			document.getElementById("word").innerHTML = "Incorrect";
 			setTimeout(clearScreen, 300);
